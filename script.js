5ff5d5ea652c7400f45789c91a30f2e7a4b164e7
Webspiro <Webspiro@users.noreply.github.com>
2020-02-20 14:59:08 -0500

    .

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.querySelectorAll('#powerMagButton').forEach(item => {
     item.addEventListener('click', addContent)
 })
 
+document.querySelectorAll('#powerMagButton').forEach(item => {
+    item.addEventListener('click', addContent)
+})
 
 
 
@@ -20,39 +23,65 @@ function showPage() {
 
 
 
-function addContent(e) {
+function addSelection(e) {
     xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        if(xhr.status = 200) {
+    xhr.onload = function () {
+        if (xhr.status = 200) {
             responseObject = JSON.parse(xhr.responseText);
-            
+
             var newContent = '';
-            //filters through magnifiers
-            for (var i = 0; i < responseObject.magnifiers.length; i++) { 
+            //filters through professionalProducts
+            for (var i = 0; i < responseObject.professionalProducts.length; i++) {
                 //execute if click event name matches object name
-                if (responseObject.magnifiers[i].name == e.target.innerText) {
-                    console.log('Matched: '+responseObject.magnifiers[i].name)
-                    newContent += '<div class="animate-bottom">';
-                    newContent += '<h1>'+responseObject.magnifiers[i].name+'</h1>';
-                    newContent += '<p>'+responseObject.magnifiers[i].description+'</p>';
-                    newContent += '<image class=\"images\" src=\"'+responseObject.magnifiers[i].image+'\">';
-                    newContent += '<div class="detailsRight">';
-                    newContent += '<h1>Features</h1>';
-                    for (var ii = 0; ii < responseObject.magnifiers[i].detailList.length; ii++) {
-                        newContent += '<li>'+responseObject.magnifiers[i].detailList[ii].item+'</li>';
-                    }
-                    newContent += '</div>';
-                    newContent += '</div>';
-                    } else {
-                        console.log('no match')
-                    }
+                if (responseObject.professionalProducts[i].name == e.target.innerText) {
+                    console.log('Matched: ' + responseObject.professionalProducts[i].name)
+                    newContent += '';
+
+                } else {
+                    console.log('no match')
+                }
 
             };
             //Add content
-            document.querySelector('.powerMagContainer').innerHTML = newContent;
+            document.querySelector('.contentBox').innerHTML = newContent;
+        }
+    };
+    xhr.open('GET', './json/mags.json', true);
+    xhr.send(null);
+}
+
+function addContent(e) {
+    xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.status = 200) {
+            responseObject = JSON.parse(xhr.responseText);
+
+            var newContent = '';
+            //filters through professionalProducts
+            for (var i = 0; i < responseObject.professionalProducts.length; i++) {
+                //execute if click event name matches object name
+                if (responseObject.professionalProducts[i].name == e.target.innerText) {
+                    console.log('Matched: ' + responseObject.professionalProducts[i].name)
+                    newContent += '<div class="animate-bottom">';
+                    newContent += '<h1>' + responseObject.professionalProducts[i].name + '</h1>';
+                    newContent += '<p>' + responseObject.professionalProducts[i].description + '</p>';
+                    newContent += '<image class=\"images\" src=\"' + responseObject.professionalProducts[i].image + '\">';
+                    newContent += '<div class="detailsRight">';
+                    newContent += '<h1>Features</h1>';
+                    for (var ii = 0; ii < responseObject.professionalProducts[i].detailList.length; ii++) {
+                        newContent += '<li>' + responseObject.professionalProducts[i].detailList[ii].item + '</li>';
+                    }
+                    newContent += '</div>';
+                    newContent += '</div>';
+                } else {
+                    console.log('no match')
+                }
+
+            };
+            //Add content
+            document.querySelector('.contentBox').innerHTML = newContent;
         }
     };
     xhr.open('GET', 'mags.json', true);
     xhr.send(null);
 }
-
